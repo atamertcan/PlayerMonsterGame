81045b495660cd752595f10959ca33eafa2b9c80
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-24 23:15:42 +0300

    Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
 
+public class Player implements ISaveable{
+    
+    private String name;
+    private String weapon;
+    private int hitPoints;
+    private int strength;
+    
+    public Player(String name, int hitPoints, int strength) {
+        
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        weapon = "Sword";
+    }
